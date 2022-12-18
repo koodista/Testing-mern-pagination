4b598edc2d8c63eff4c83be4e2a5a9321a6301b2
Koodista <koodista@gmail.com>
2022-12-18 06:32:44 +0200

    backend pagination

--- backend/controllers/bikejourneyControllers.js
+++ backend/controllers/bikejourneyControllers.js
@@ -5,11 +5,35 @@ const ErrorResponse = require('../utils/errorResponse')
 
 
 exports.getAllBikejourneys = asyncHandler(async (req, res, next) => {
-  const bikejourneys = await Bikejourney.find();
+  //const bikejourneys = await Bikejourney.find();
+
+  let query = Bikejourney.find();
+
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.limit) || 50;
+  const skip = (page - 1) * pageSize;
+  const total = await Bikejourney.countDocuments();
+
+  const pages = Math.ceil(total / pageSize);
+
+  query = query.skip(skip).limit(pageSize);
+
+  if(page > pages) {
+    return res.status(404).json({
+      status: 'Failed',
+      message: 'No page found'
+    })
+  }
+
+  const result = await query;
 
   res.status(200).json({
-    success: true,
-    data: bikejourneys
+      status: 'success',                //success: true,
+      count: result.length,             //data: bikejourneys
+      page,
+      pages,
+      data: result
+                                                                                  
   })
 });
 
